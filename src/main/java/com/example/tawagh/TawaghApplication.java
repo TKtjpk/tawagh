f9efc986b47097bc6fbae2fde3680bc5625319f2
Tomasz <80661109+TKtjpk@users.noreply.github.com>
2023-06-21 20:14:17 +0200

    Initial commit

---
+++ src/main/java/com/example/tawagh/TawaghApplication.java
@@ -0,0 +1,13 @@
+package com.example.tawagh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawaghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TawaghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tawagh/TawaghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tawagh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawaghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
